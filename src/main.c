e82c75cef3e7155bbb16502d4cb9a4e5506f59e9
Dom Rodriguez <shymega@shymega.org.uk>
2020-08-12 20:22:52 +0100

    [code]: Move back to C, and expand on SSID scanner prototype

--- src/main.c
+++ src/main.c
@@ -2,50 +2,76 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <glib-2.0/glib.h>
-#include <glib-2.0/gio/gio.h>
+#include "utils.h"
 
-#include <libnm/NetworkManager.h>
-#include <NetworkManager.h>
+NMClient* create_nm_client() {
+    NMClient *client;
+    GError *error = NULL;
 
-#include <string.h>
+    g_info ("Now initialising...");
+    g_debug ("INIT: NetworkManager client.");
+
+    if ((client = nm_client_new (NULL, &error)) == 0) {
+        g_critical ("Error creating NMClient. Please report issue.");
+        g_critical ("Further information is here: %s", error->message);
+        g_error_free (error);
+
+        /* we can't continue now. */
+        exit (EXIT_FAILURE);
+    }
+
+    g_debug ("INIT: Client created.");
+
+    return client;
+}
+
+void check_nm_client (NMClient *client) {
+    if (client == NULL) {
+        g_critical ("NULL NM Client variable detected! PANIC!");
+        exit (EXIT_FAILURE);
+    }
+
+    if (nm_client_get_nm_running (client) == -1) {
+        g_critical ("NetworkManager is NOT running! Halt.");
+        exit (EXIT_FAILURE);
+    }
+}
 
 int
 main (void) {
     NMClient *client;
-    GError *error = NULL;
-    const GPtrArray *conns;
+    const GPtrArray *devices;
     guint i;
 
-    if (! (client = nm_client_new (NULL, &error))) {
-        g_message ("Error: Could NOT connect to NetworkManager: %s.",
-            error->message);
-        g_error_free (error);
+    g_log_set_default_handler(g_log_writer_default(G_LOG_LEVEL_INFO, NULL, NULL, NULL));
+
+    client = create_nm_client ();
+    check_nm_client (client);
+
+    devices = nm_client_get_all_devices (client);
+
+    if (devices == NULL || devices->len <= 0) {
+        g_critical ("No network devices detected.");
         return EXIT_FAILURE;
     }
 
-    if (! nm_client_get_nm_running (client)) {
-        g_message ("Error: Can NOT obtain connections: NetworkManager is not running.");
-        return EXIT_FAILURE;
-    }
-
-    conns = nm_client_get_active_connections (client);
-    printf ("Connections:\n============\n");
-
-    for (i = 0; i < conns->len; i++) {
-        NMActiveConnection *conn;
-        const char *uuid, *id;
-        const GPtrArray *devices;
+    for (i = 0; i < devices->len; i++) {
         NMDevice *device;
-        const GPtrArray *accessPoints;
+        const GPtrArray *access_points;
         guint ap;
 
-        conn = conns->pdata[i];
-        uuid = nm_active_connection_get_uuid (conn);
-        id = nm_active_connection_get_id (conn);
+        device = devices->pdata[i];
 
-        devices = nm_active_connection_get_devices (conn);
-        device = g_ptr_array_index (devices, 0);
+        if (device == NULL) {
+            g_error ("NULL value detected. (Device). Exiting.");
+            return EXIT_SUCCESS;
+        }
+
+        if (! NM_IS_DEVICE_WIFI (device)) {
+            /* this device isn'ts not a WiFi device.
+             * No need to emit anything. */
+            continue;
+        }
 
         if (NM_IS_DEVICE_WIFI (device)) {
             nm_device_wifi_request_scan(
@@ -53,44 +79,42 @@ main (void) {
                 NULL,
                 NULL);
 
-            printf("Sleeping to allow NetworkManager to update.\n");
-            sleep(2); /* we should wait for NM to update */
+            access_points = nm_device_wifi_get_access_points (NM_DEVICE_WIFI (device));
 
-            accessPoints = nm_device_wifi_get_access_points (NM_DEVICE_WIFI (device));
-
-            for (ap = 0; ap < accessPoints->len; ap++) {
-                NMAccessPoint *accessPoint;
-                GBytes *accessPoint_SSID_GB;
+            for (ap = 0; ap < access_points->len; ap++) {
+                NMAccessPoint *access_point;
+                GBytes *access_point_SSID_GBytes;
                 gsize len;
                 guint8 *bytes;
-                char *accessPoint_SSID;
+                guint8 signal_strength;
+                char *access_point_SSID_str;
+                const char *access_point_BSSID_str;
+                const char *bars;
 
-                accessPoint = accessPoints->pdata[ap];
-                accessPoint_SSID_GB = nm_access_point_get_ssid (accessPoint);
-                if (accessPoint_SSID_GB == NULL) {
-                    /* this is OK to have as a value
-                     *
-                     * we just silently handle it, and
-                     * iterate to the next element in
-                     * the array. */
+                access_point = access_points->pdata[ap];
+                access_point_SSID_GBytes = nm_access_point_get_ssid(access_point);
+
+                if (access_point_SSID_GBytes == NULL) {
+                    /* this is an empty result
+                     * not an error, continue */
                     continue;
                 }
 
-                bytes = (guint8*)g_bytes_unref_to_data (accessPoint_SSID_GB, &len);
-                accessPoint_SSID = nm_utils_ssid_to_utf8 (bytes, len);
+                signal_strength = nm_access_point_get_strength (access_point);
+                access_point_BSSID_str = nm_access_point_get_bssid (access_point);
+                bytes = g_bytes_unref_to_data (access_point_SSID_GBytes, &len);
+                access_point_SSID_str = nm_utils_ssid_to_utf8(bytes, len);
+                bars = nm_utils_wifi_strength_bars (signal_strength);
 
-                printf ("SSID: %s | UUID: %s | ID: %s | WAP BSSID: %s\n",
-                        accessPoint_SSID,
-                        uuid,
-                        id,
-                        nm_access_point_get_bssid (accessPoint));
+                g_print ("WAP SSID: %s || WAP BSSID: %s || WAP strength: %d%% || Bars: %s\n",
+                        access_point_SSID_str,
+                        access_point_BSSID_str,
+                        signal_strength,
+                        bars);
             }
-
         }
 
     }
-
-    g_object_unref (client);
 
     return EXIT_SUCCESS;
 }
